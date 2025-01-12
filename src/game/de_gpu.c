784956b5743f5b61b852c2a7cc479115d292d312
Mateusz Garbowski <fda0@matg.pl>
2025-01-12 15:51:22 +0100

    make teapot bigger

--- src/game/de_gpu.c
+++ src/game/de_gpu.c
@@ -356,7 +356,7 @@ static void Gpu_Iterate()
         multiply_matrix(matrix_rotate, matrix_modelview, matrix_modelview);
 
         /* Pull the camera back from the cube */
-        matrix_modelview[14] -= 2.5f * 10.f;
+        matrix_modelview[14] -= 2.5f * 3.f;
 
         perspective_matrix(45.0f, (float)draw_width/draw_height, 0.01f, 100.0f, matrix_perspective);
         multiply_matrix(matrix_perspective, matrix_modelview, (float*) &matrix_final);
