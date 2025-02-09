2da67d061ee381a8fa22f49f2e134ab972fb5d7b
Mateusz Garbowski <fda0@matg.pl>
2025-02-09 16:04:00 +0100

    enable teapot-on-teapot collision

--- src/game/game_object.c
+++ src/game/game_object.c
@@ -127,7 +127,7 @@ static Object *Obj_Create(Obj_Category storage, U32 flags)
 static Object *Obj_CreateWall(V2 p, V2 dim)
 {
     Object *obj = Obj_Create(ObjStorage_Local,
-                                ObjFlag_Draw|ObjFlag_Collide);
+                             ObjFlag_Draw|ObjFlag_Collide);
     obj->s.p = p;
     obj->s.collision.verts = CollisionVertices_FromRectDim(dim);
     Collision_RecalculateNormals(&obj->s.collision);
@@ -146,10 +146,10 @@ static Object *Obj_CreateWall(V2 p, V2 dim)
 static Object *Obj_CreatePlayer()
 {
     Object *player = Obj_Create(ObjStorage_Net,
-                                   ObjFlag_Draw|ObjFlag_Move|
-                                   /*|ObjFlag_Collide*/
-                                   ObjFlag_ModelTeapot |
-                                   ObjFlag_AnimateRotation);
+                                ObjFlag_Draw | ObjFlag_Move |
+                                ObjFlag_Collide |
+                                ObjFlag_ModelTeapot |
+                                ObjFlag_AnimateRotation);
 
     player->s.collision.verts = CollisionVertices_FromRectDim((V2){30, 30});
     Collision_RecalculateNormals(&player->s.collision);
