c98fc0fdf0175e51d869444d07d081ee53103662
Mateusz Garbowski <fda0@matg.pl>
2025-05-28 23:30:16 +0200

    [jai] fix CalculateMipMapCount

--- src/base/base_hash.h
+++ src/base/base_hash.h
@@ -37,20 +37,3 @@ static U64 HashEnd(HashState *state)
   U64 res = MeowU64From(hash, 0);
   return res;
 }
-
-#if 0 // old xxhash version
-// Wrappers around xxhash.h
-#define XXH_STATIC_LINKING_ONLY
-#define XXH_IMPLEMENTATION
-#include "xxhash.h"
-
-static U64 U64_Hash(U64 seed, void *data, U64 size)
-{
-  return XXH3_64bits_withSeed(data, size, seed);
-}
-
-static U64 S8_Hash(U64 seed, S8 string)
-{
-  return U64_Hash(seed, string.str, string.size);
-}
-#endif
